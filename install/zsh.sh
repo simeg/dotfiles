7892a9a39122bb8fa8056f854b20c818b84fa902
Simon Egersand <s.egersand@gmail.com>
2017-07-01 13:26:54 +0200

    [zsh] Do not sanity check for brew
    Brew should be installed before, so it should not be necessary.

--- install/zsh.sh
+++ install/zsh.sh
@@ -2,12 +2,7 @@
 
 set -e
 
-# Installs zsh with plugins using brew.
-
-# Sanity check. Is brew installed on the system? If not - install it
-type brew >/dev/null 2>&1 || {
-  echo >&2 "Could not find brew. Is it installed?"; exit 1;
-}
+# Installs zsh with plugins using brew, assumes brew is installed
 
 /usr/local/bin/brew install \
   z \
