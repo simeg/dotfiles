cf5367fcf04d5b26bd45e1299ca4f64d5af04bca
Simon Egersand <s.egersand@gmail.com>
2017-07-01 13:37:49 +0200

    Do not sanity check for brew, it should be installed

--- install/other.sh
+++ install/other.sh
@@ -5,13 +5,6 @@ set -e
 # Installs brew packages that does not belong to any
 # application, hence the name 'other'.
 
-# Sanity check. Is brew installed on the system? If not - install it
-type brew >/dev/null 2>&1 || {
-  echo >&2 "Could not find brew. Installing it";
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
-  exit 1;
-}
-
 /usr/local/bin/brew install \
   git \
   node \
