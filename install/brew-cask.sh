c7228d662e5a66cfa91dee3b65217a45999119ed
Simon Egersand <s.egersand@gmail.com>
2017-07-01 13:57:23 +0200

    [brew-cask] Install all apps I use that I can find
    I was not aware of brew cask until yesterday. I've been looking for
    something like it, now I don't have to remember to install all of these
    applications all the time. Great success!

--- install/brew-cask.sh
+++ install/brew-cask.sh
@@ -14,7 +14,22 @@ set -e
   qlvideo \
   google-chrome \
   atom \
+  docker \
   docker-toolbox \
+  alfred \
+  vlc \
+  spectacle \
+  bartender \
+  appzapper \
+  boom \
+  dashlane \
+  dropbox \
+  iterm2 \
+  slack \
+  steam \
+  unrarx \
+  the-unarchiver \
+  windscribe \
 &&
 echo "Brew cask setup complete, a reboot is needed"
 
