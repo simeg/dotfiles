cf1d7871230fea4846c2758749f363e105c2ad1c
Simon Egersand <s.egersand@gmail.com>
2018-05-08 22:01:41 +0200

    Make setup script less strict
    Before it was checking if any files existed before symlinking them, and
    in that case creating a backup. I feel like it was a bit too strict and
    hence I removed it.

--- setup.sh
+++ setup.sh
@@ -1,43 +1,6 @@
 #!/bin/sh
-#
-# Creates symlinks from all dotfiles in this
-# folder to $HOME. If a dotfile already exists
-# then that is backed up before a symlink is
-# created. Also runs all setup scripts to
-# install plugins.
-
-#############################################
-# Utility functions
-#############################################
-
-# Returns file name from path.
-# Assumes input looks like: "folder/file"
-function get_file_name {
-  echo $(sed 's/.*\///g' <<< $1);
-}
-
-# Checks if file exists in root
-function file_exist {
-  FILE=$(get_file_name $1);
-
-  if [ -f "$HOME/$FILE" ]; then
-    # 0 = true
-    return 0
-  else
-    # 1 = false
-   return 1
-  fi
-}
-
-#############################################
-# Symlinks
-#############################################
 
 source ./symlink.sh
-
-#############################################
-# Plugins
-#############################################
 
 source ./install/package-managers.sh
 source ./install/brew-cask.sh
@@ -49,12 +12,7 @@ source ./install/vim.sh
 source ./install/safe-rm/safe-rm.sh
 source ./install/macOS/macOS.sh
 
-#############################################
-# Etc
-#############################################
-
-# Do not display "Last logged in ..." when
-# creating new shell
+# Do not display "Last logged in ..." when creating new shell
 if [ ~/.hushlogin ]; then
   echo "~/.hushlogin already exist, will not create a new one";
 else
