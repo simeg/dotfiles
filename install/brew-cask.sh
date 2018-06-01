67cde1dcc4a2f70ef016b17aa3581433087cfd44
Simon Egersand <s.egersand@gmail.com>
2018-06-01 23:59:55 +0200

    [brew] Add descriptions to cask apps

--- install/brew-cask.sh
+++ install/brew-cask.sh
@@ -5,31 +5,31 @@ set -e
 # Installs brew cask packages, assumes brew is installed
 
 /usr/local/bin/brew cask install \
-  qlmarkdown \
-  quicklook-json \
-  qlcolorcode \
-  qlstephen \
-  betterzipql \
-  qlimagesize \
-  qlvideo \
-  google-chrome \
-  atom \
-  docker \
-  docker-toolbox \
-  alfred \
-  vlc \
-  spectacle \
-  bartender \
-  appzapper \
-  boom \
-  dashlane \
-  dropbox \
-  iterm2 \
-  slack \
-  steam \
-  unrarx \
-  the-unarchiver \
-  windscribe \
+  qlmarkdown \                       # Quick look Finder plugin
+  quicklook-json \                   # Quick-look Finder plugin
+  qlcolorcode \                      # Quick-look Finder plugin
+  qlstephen \                        # Quick-look Finder plugin
+  betterzipql \                      # Quick-look Finder plugin
+  qlimagesize \                      # Quick-look Finder plugin
+  qlvideo \                          # Quick-look Finder plugin
+  google-chrome \                    # Browser
+  atom \                             # Editor
+  docker \                           # Virtual environment
+  docker-toolbox \                   # Docker utilities
+  alfred \                           # Spotlight with workflows
+  vlc \                              # Media streamer
+  spectacle \                        # Window management
+  bartender \                        # Menu bar item controller
+  appzapper \                        # App remover
+  boom \                             # Audio enhancer
+  dashlane \                         # Password manager
+  dropbox \                          # Cloud storage
+  iterm2 \                           # Alternative to Terminal
+  slack \                            # Communication platform
+  steam \                            # Gaming platform
+  unrarx \                           # Decompression tool
+  the-unarchiver \                   # Decompression tool
+  windscribe \                       # VPN
 &&
 echo "Brew cask setup complete, a reboot is needed"
 
