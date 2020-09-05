2567fccac0e79fdfaa3d7d7d22522f71c7cc6f3d
Simon Egersand <s.egersand@gmail.com>
2020-09-05 18:02:26 +0200

    Shellcheck fixes

--- symlink.sh
+++ symlink.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 symlink_file_to_home() {
-  ln -sv "`pwd`/$1" $HOME
+  ln -sv "$(pwd)/$1" "$HOME"
 }
 
 for dotfile in */.*; do
@@ -11,11 +11,6 @@ for dotfile in */.*; do
   fi
 done
 
-ln -sv `pwd`/vim "$HOME"/.vim
-ln -sv `pwd`/bin "$HOME"/.bin
+ln -sv "$(pwd)"/vim "$HOME"/.vim
+ln -sv "$(pwd)"/bin "$HOME"/.bin
 
-# istat-menus
-readonly ISTAT_CONF_FILE_PATH=`pwd`/istat-menus/com.bjango.istatmenus6.extras.plist
-readonly ISTAT_CONF_FOLDER_PATH=/Users/simon/Library/Preferences
-
-#ln -sv ${ISTAT_MENU_CONFIG_FILE_PATH} ${ISTAT_CONF_FOLDER_PATH}
