f9697e6af345fd597cde632e7a96b4ce944c0f6b
Simon Egersand <s.egersand@gmail.com>
2025-08-24 08:01:31 +0200

    In nvim, when suggestion, use esc to decline it
    Before this change, I could use escape to decline but then it would also
    take me out of the mode I was in

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -25,6 +25,7 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
+    ['<Esc>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
     ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
