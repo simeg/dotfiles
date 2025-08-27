3c18f149c5a40184fdf1bc63cb818537b003afe7
Simon Egersand <s.egersand@gmail.com>
2025-08-27 06:39:16 +0200

    Ctrl+x ctrl+e opens up cmd in nvim

--- .config/zsh/keybindings.zsh
+++ .config/zsh/keybindings.zsh
@@ -4,7 +4,12 @@
 # Make slash a word boundary so ⌥⌫ kills just the last path segment
 typeset -g WORDCHARS='*?[]~=&;!#$%^(){}<>'
 
+# Enable edit-command-line widget and bind to Ctrl+X Ctrl+E
+autoload -Uz edit-command-line
+zle -N edit-command-line
+bindkey '^X^E' edit-command-line
+
 # (Optional) ensure Meta+Backspace maps to backward-kill-word
-# Only add if you’ve customized keymaps; most setups don’t need this.
+# Only add if you've customized keymaps; most setups don't need this.
 #bindkey -M emacs '^[^?' backward-kill-word
 #bindkey -M viins '^[^?' backward-kill-word
